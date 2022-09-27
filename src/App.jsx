9c801dbf52ab605203c46a9f623d44fac9c40989
Nicolas Wolf <nico.wolf@outlook.com>
2022-09-27 01:40:15 -0300

    Add sass + test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./App.module.scss";
 
 const App = () => {
-  return <h1>Nico portfolio</h1>;
+  return <h1 className={classes.main}>Nico portfolio</h1>;
 };
 
 export default App;
